10a558d166a2a13d908ff27d4340deabb1faa48b
Jh3xy <jhxydev.me@gmail.com>
2025-10-26 13:02:15 -0700

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,14 @@
 
-// Check task Functionality
+// Grab Elements
+
 let checkboxes = document.querySelectorAll('.checkbox')
+let  btn = document.querySelector('.add-task-btn')
+let taskInput = document.getElementById('task-input')
+let taskLists = document.querySelector('.list__tasks')
+// console.log(taskLists)
+
+
+// Check task Functionality
 
 checkboxes.forEach(
     (checkbox) =>{
@@ -13,3 +21,52 @@ checkboxes.forEach(
         })
     }
 )
+
+// Add Task Functionality
+
+btn.addEventListener("click", () => {
+    if (taskInput.value === '') {
+        console.log("invalid");
+    } else {
+        // Get taskInput current value
+        let newTask = taskInput.value;
+        console.log(newTask)
+
+        // Create todo elements
+
+        let newList = document.createElement('li') //create list tag
+        newList.classList.add('list__task', 'flex') //Add classes
+
+        let checkbox = document.createElement('span') //Create checkbox
+         checkbox.classList.add('checkbox') //Add class
+         newList.appendChild(checkbox)
+         
+         let task = document.createElement('span') //Create Task Description
+         task.classList.add('task__description') //Add class
+         task.innerHTML = taskInput.value
+         newList.appendChild(task)
+         
+         let deleteBtn = document.createElement('img')
+         deleteBtn.setAttribute('src', './images/icon-cross.svg')
+         deleteBtn.setAttribute('alt', 'delete task')
+         newList.appendChild(deleteBtn)
+
+        //  Add newList to DOM
+
+    }
+});
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
